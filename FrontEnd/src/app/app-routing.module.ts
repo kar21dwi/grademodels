e18ed422d2f84f273bb1aa161e39758faaa99e00
kar21dwi <kar21dwi@gmail.com>
2019-07-04 20:16:51 +0530

    Uis-pushed

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { GradesequenceinputComponent } from './components/inputs/gradesequenceinput/gradesequenceinput.component';
 import { HdpeinputComponent } from './components/inputs/gradesequenceinput/hdpeinput/hdpeinput.component';
 import { NcuparametersComponent } from './components/inputs/gradesequenceinput/hdpeinput/commonparameters/ncuparameters/ncuparameters.component';
+import { HdpeoutputComponent } from './components/outputs/gradesequenceoutput/hdpeoutput/hdpeoutput.component';
 
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'gradesequence', component: GradesequenceinputComponent },
 	{ path: 'hdpeinput', component: HdpeinputComponent },
-	{ path: 'ncuparameters', component: NcuparametersComponent }
+	{ path: 'ncuparameters', component: NcuparametersComponent },
+	{ path: 'hdpecurrentoutput', component: HdpeoutputComponent }
 ];
 
 @NgModule({
